9bf7421b9aedbe4f67c3f8b0ee5504a8f9fedb37
aruna <arunajava567@gmail.com>
2021-05-17 18:19:20 +0530

    again commit

--- springApp1/src/main/java/com/capgemini/springApp1/SpringApp1Application.java
+++ springApp1/src/main/java/com/capgemini/springApp1/SpringApp1Application.java
@@ -8,7 +8,7 @@ public class SpringApp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApp1Application.class, args);
-		//System.out.println("Hello123sadsawith credentials ");
+		//System.out.println("Hello123sadsawith credentialsdfdsfdfdsf ");
 	}
 
 }
